aae6d1af60d6ea05a91bf9288cfe5086492c9087
ShashankMangal <shashankmangal10@gmail.com>
2022-06-11 17:58:33 +0530

    Bottom Navigation

--- app/src/main/java/com/sharkBytesLab/modernMirchiRadio/MainActivity.java
+++ app/src/main/java/com/sharkBytesLab/modernMirchiRadio/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+import com.sharkBytesLab.modernMirchiRadio.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNav.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+
+
+
+            }
+        });
+
     }
 }
