539116067088629b934ff44bbc83311e5aa6af7e
ShashankMangal <shashankmangal10@gmail.com>
2022-06-11 18:20:26 +0530

    Bottom Navigation working with fragment

--- app/src/main/java/com/sharkBytesLab/modernMirchiRadio/MainActivity.java
+++ app/src/main/java/com/sharkBytesLab/modernMirchiRadio/MainActivity.java
@@ -1,15 +1,20 @@
 package com.sharkBytesLab.modernMirchiRadio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+import com.sharkBytesLab.modernMirchiRadio.Fragments.HomeFragment;
+import com.sharkBytesLab.modernMirchiRadio.Fragments.SettingFragment;
 import com.sharkBytesLab.modernMirchiRadio.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +22,36 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        if(savedInstanceState == null)
+        {
+            binding.bottomNav.setItemSelected(R.id.home, true);
+            fragmentManager = getSupportFragmentManager();
+            HomeFragment homeFragment = new HomeFragment();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
+        }
+
         binding.bottomNav.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
             @Override
             public void onItemSelected(int i) {
 
+                Fragment fragment = null;
 
+                switch(i)
+                {
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+
+                    case R.id.settings:
+                        fragment = new SettingFragment();
+                        break;
+                }
+
+                if(fragment!=null)
+                {
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                }
 
             }
         });
